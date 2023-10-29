13bb23c18c65dd9ddee22b22642e335cf128608c
jjones <jimmy.jones@tutanota.com>
2023-10-29 11:34:17 -0600

    parse raw data and store in data store

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
 	import type { LayoutData } from './$types';
+	import { setContext } from 'svelte';
+	import * as d3 from 'd3';
+
+	import { migrantsDataStore } from '$lib/stores/migrant-data';
 
 	export let data: LayoutData;
 
-	console.log('Layout.svelte:: ', data.rawMigrantData);
+	$: migrantsDataStore.set({
+		migrants: d3.csvParse(data.rawMigrantData, d3.autoType)
+	});
+
+	setContext('migrants', migrantsDataStore);
 </script>
+
+<slot />
