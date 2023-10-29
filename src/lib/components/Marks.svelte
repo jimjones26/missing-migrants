a5bcb946e7df2a70fe5329ee0bc5378c5c5446f8
jjones <jimmy.jones@tutanota.com>
2023-10-29 15:24:31 -0600

    add marks to chart

---
+++ src/lib/components/Marks.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let data: Array<MigrantDataItem>;
+	export let yScale: any;
+	export let yValue: any;
+	export let xScale: any;
+	export let xValue: any;
+	export let circleRadius: number;
+	export let opacity: number;
+</script>
+
+{#each data as item, i}
+	<circle
+		cx={xScale(xValue(item))}
+		cy={yScale(yValue(item))}
+		r={circleRadius}
+		fill="#137B80"
+		{opacity}
+	>
+		<title>hover</title>
+	</circle>
+{/each}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -47,7 +47,15 @@
 	<g transform={`translate(${margin.left}, ${margin.top})`}>
 		<AxisY {yScale} {innerWidth} tickOffset={10} />
 		<AxisX {xScale} {innerHeight} tickOffset={10} {formatTime} />
-		<Marks />
+		<Marks
+			data={$migrantsData.migrants}
+			{xScale}
+			{yScale}
+			{yValue}
+			{xValue}
+			circleRadius={3}
+			opacity={0.5}
+		/>
 		<text text-anchor="middle" transform={`translate(${-45}, ${innerHeight / 2}) rotate(-90)`}
 			>{yAxisLabel}</text
 		>
